aa41063181140bfda6116d963a0c5e86bd59005f
sumit715-su8een <su8eenbrand715@gmail.com>
2025-11-25 12:44:46 +0530

    Create Q52.c

---
+++ Q52.c
@@ -0,0 +1,48 @@
+/*
+ Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= (2*i) - 1; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 3; j >= (2*i) - 1; j--) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
