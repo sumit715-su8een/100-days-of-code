70acf6e95325e924a5482576957b4e928ae994dc
sumit715-su8een <su8eenbrand715@gmail.com>
2025-11-25 13:03:23 +0530

    Create Q72.c

---
+++ Q72.c
@@ -0,0 +1,36 @@
+//Q72: Find the sum of all elements in a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+21
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int arr[rows][cols];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum += arr[i][j];
+        }
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
