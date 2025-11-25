67182581f3fd1916b3258c5219fb27931b681603
sumit715-su8een <su8eenbrand715@gmail.com>
2025-11-25 12:35:46 +0530

    Create Q27.c

---
+++ Q27.c
@@ -0,0 +1,15 @@
+//Q27: Write a program to print the sum of the first n odd numbers.
+
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    int sum = 0;
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++) {
+            sum += (2*i)-1;
+    }
+    printf("%d",sum);
+    return 0;
+}
